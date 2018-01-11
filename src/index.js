9b212339d76b2b05a3be88db00a077e3a94cc9c7
Ives van Hoorne <Ives.v.h@gmail.com>
2018-01-11 17:44:59 +0100

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center"
 };
 
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+    <h2>Some edits magic happen {"\u2728"}</h2>
   </div>
 );
 
